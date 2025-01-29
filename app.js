1f369b72ff6c205f23ef4283b9d74059a6a0cc99
Santosh <mailsantosh77@gmail.com>
2025-01-29 18:12:58 +0530

    Initial commit of javainstaller project

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+app.use(express.json());
+
+app.use("/api", routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
